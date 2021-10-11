0086d1f8dd17d641547bb24a119c3648a14e0913
Zane Walsh <zwalsh@usf.edu>
2021-10-10 22:24:06 -0400

    Add project files.

---
+++ FromAtoB/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace FromAtoB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            int a = int.Parse(input[0]);
+            int b = int.Parse(input[1]);
+
+            // int a = int.Parse(Console.ReadLine());
+            // int b = int.Parse(Console.ReadLine());
+            int counter = 0;
+            while (a != b)
+            {
+
+                if (a > b)
+                {
+                    while (a > b)
+                    {
+                        if (a % 2 == 0)
+                        {
+                            a = a / 2;
+                            counter++;
+                        }
+                        else
+                        {
+                            a++;
+                            counter++;
+                        }
+                    }
+                }
+                else
+                {
+                    // while(a < b){
+                    //     a++;
+                    //     counter++;
+                    // }
+                    break;
+                }
+            }
+            if (a < b)
+            {
+                counter += b - a;
+            }
+            Console.WriteLine(counter);
+        }
+    }
+}
